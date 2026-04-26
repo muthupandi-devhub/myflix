ef4d21c6a6330e672556d2dfd57c84139ad8a7ff
MUTHU-HUB-DEV <muthupandi030605@gmail.com>
2026-04-26 21:18:48 +0530

    just normal update

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ app.post("/login", (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+app.listen(3000, () => {
+  console.log("Server running on.....");
 });
